c3818e16e180ddac2fb6a09c8bcad482c7c3964e
Cyrille MÉDARD DE CHARDON <cyrille.medarddechardon@uni.lu>
2022-10-26 23:08:36 +0200

    added satellite views

---
+++ data/nmea_extract_latlng_and_sv.py
@@ -0,0 +1,71 @@
+import sys
+import operator
+from functools import reduce
+
+if len(sys.argv) < 2:
+    print("Please provide the filename(s) you would like to process:\n" +
+    "python3 nmea_extract_latlng.py nmea_file.csv\n" +
+    "python3 nmea_extract_latlng.py f1.csv f2.csv\n")
+
+# go through each file
+for fn in sys.argv[1:]:
+    # save data to two files - latlng and satviews
+    fll = open(fn.split('.')[0] + '_latlng.csv', 'w')
+    fll.write('rid,time,lat,lng\n')
+    fsv = open(fn.split('.')[0] + '_sv.csv', 'w')
+    fsv.write('rid,time,satnum,elev,azim,SNR\n')
+
+    # open file for reading
+    with open(fn, 'r') as fr:
+
+        # count the lines and save the last time observed
+        rcount = 0
+        time = ''
+
+        # go through each line
+        for line in fr:
+            # split csv into parts
+            p = line.strip().split(',')
+
+            # GNSS Satellite views (locations)
+            if p[0] == '$GPGSV':
+                # validate checksum (https://gist.github.com/MattWoodhead/0bc2b3066796e19a3a350689b43b50ab)
+                nmeapckg, checksum = line.strip('$\n').split('*')
+                calc_checksum = reduce(operator.xor, (ord(s) for s in nmeapckg), 0)
+                if int(checksum, base=16) != calc_checksum:
+                    continue
+
+                p = nmeapckg.split(',')
+                peat = p[4:]
+
+                # can be up to 4 SV per line - consume the first four parts
+                while len(peat) >= 4:
+                    # check each value
+                    # write to file
+                    fsv.write(str(rcount) + ',' + time + ',' + peat[0] + ',' + peat[1] + ',' + peat[2] + ',' + peat[3] + '\n')
+                    peat = peat[4:]
+
+            # GNSS Fix Data
+            if p[0] == '$GPGGA':
+
+                # validate
+                # skip badly formed sentences
+                if len(p) != 15 or p[2] == '' or p[4] == '':
+                    continue
+
+                time = p[1][0:2] + ':' + p[1][2:4] + ':' + p[1][4:6] + ' Z'
+
+                # convert the lat/lng from deg-min to dec-deg
+                lat = float(int(p[2][0:2])) + float(p[2][2:])/60
+                if p[3] == 'S':
+                    lat = -lat
+
+                lng = float(p[4][0:3]) + float(p[4][3:])/60
+                if p[5] == 'W':
+                    lng = -lng
+
+                #print(rcount, time, lat, lng)
+                fll.write(str(rcount) + ',' + time + ',' + str(lat) + ',' + str(lng) + '\n')
+                rcount+=1
+fll.close()
+fsv.close()
