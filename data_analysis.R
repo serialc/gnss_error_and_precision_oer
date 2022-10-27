2d484a56ff25738e287db8136790f3f476fe33c4
serialc <cyrille@gmail.com>
2022-10-27 14:14:21 +0200

    streamlined

--- data_analysis.R
+++ data_analysis.R
@@ -3,32 +3,45 @@ library(plotrix) # for plotting circles
 library(RColorBrewer)
 library(TeachingDemos)
 
-sv <- read.table('data/SD00262-GPS-0186_walk_Belval_PR_to_MSH_sv.csv', sep=',', header = TRUE)
+# function, takes a path to a SV file and creates the figure
+plotSats <- function(source_file) {
+  print(source_file)
+  sv <- read.table(paste0('processing/sv_data/', source_file), sep=',', header = TRUE)
+  
+  # replace SNR NA values with 0
+  sv$SNR[is.na(sv$SNR)] <- 0
+  # convert time to date
+  sv$time <- strptime(sv$time, format="%H:%M:%S", tz = "UTC")
+  str(sv)
+  
+  # I'm not sure I should add the elev as linear and not transformed (sin/cos?)...
+  sv$pathx <- sin(sv$azim/180*pi)*(90-sv$elev)
+  sv$pathy <- cos(sv$azim/180*pi)*(90-sv$elev)
+  
+  # get a bunch of unique colours
+  qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+  col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+  
+  png(filename = paste0('images/', strsplit(source_file, "[.]")[[1]][1], "_satpaths.png"), width=500, height=500)
+  par(mar=rep(0,4))
+  plot(NULL, xlim=c(-90,90), ylim=c(-90,90), pch=21, cex=sv$SNR/30, bg=col_vector[sv$satnum], asp=1, lwd=3, col="black")
+  lines(c(0,0), c(-90,90))
+  lines(c(-90,90), c(0,0))
+  draw.circle(0,0,90,100)
+  draw.circle(0,0,45,100)
+  sapply(split(sv, sv$satnum), function(s) {
+    points(s$pathx, s$pathy, pch=21, cex=s$SNR/30, col="black", lwd=3)
+    points(s$pathx, s$pathy, pch=19, cex=s$SNR/30, col=col_vector[s$satnum], asp=1)
+  })
+  shadowtext(c(0,0,0),c(0,-45,-90), labels = c("90°\nZenith","45°","0°\nHorizon"), bg="white", col="black", r=0.1)
+  shadowtext(c(0,70,0,-70), c(70,0,-70,0), labels = c("N", "E", "S", "W"), bg="white", col="black", r=0.1)
+  dev.off()
+}
+         
+# will render figure for all data sets in the path below and save it to 'images'
+for(sf in list.files('processing/sv_data/')) {
+  plotSats(sf)
+}
 
-# replace SNR NA values with 0
-sv$SNR[is.na(sv$SNR)] <- 0
-# convert time to date
-sv$time <- strptime(sv$time, format="%H:%M:%S", tz = "UTC")
-str(sv)
-
-# I'm not sure I should add the elev as linear and not transformed (sin/cos?)...
-sv$pathx <- sin(sv$azim/180*pi)*(90-sv$elev)
-sv$pathy <- cos(sv$azim/180*pi)*(90-sv$elev)
-
-# get a bunch of unique colours
-qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
-col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
-
-par(mar=rep(0,4))
-
-plot(NULL, xlim=c(-90,90), ylim=c(-90,90), pch=21, cex=sv$SNR/30, bg=col_vector[sv$satnum], asp=1, lwd=3, col="black")
-lines(c(0,0), c(-90,90))
-lines(c(-90,90), c(0,0))
-draw.circle(0,0,90,100)
-draw.circle(0,0,45,100)
-sapply(split(sv, sv$satnum), function(s) {
-  points(s$pathx, s$pathy, pch=21, cex=s$SNR/30, col="black", lwd=3)
-  points(s$pathx, s$pathy, pch=19, cex=s$SNR/30, col=col_vector[s$satnum], asp=1)
-})
-shadowtext(c(0,0,0),c(0,-45,-90), labels = c("90°\nZenith","45°","0°\nHorizon"), bg="white", col="black", r=0.1)
-shadowtext(c(0,70,0,-70), c(70,0,-70,0), labels = c("N", "E", "S", "W"), bg="white", col="black", r=0.1)
+  
+                                                                                                                                    
--- processing/nmea_extract_latlng_and_sv.py
+++ processing/nmea_extract_latlng_and_sv.py
@@ -1,22 +1,30 @@
+import os
 import sys
 import operator
 from functools import reduce
 
+nmeadp = 'nmea_data/'
+svdp = 'sv_data/'
+lldp = 'll_data/'
+
 if len(sys.argv) < 2:
-    print("Please provide the filename(s) you would like to process:\n" +
+    print("Please provide the path and filename(s) you would like to process:\n" +
     "python3 nmea_extract_latlng.py nmea_file.csv\n" +
-    "python3 nmea_extract_latlng.py f1.csv f2.csv\n")
+    "python3 nmea_extract_latlng.py nmea/f1.csv f2.csv\n")
 
 # go through each file
-for fn in sys.argv[1:]:
+for fp in sys.argv[1:]:
+    # extract the filename
+    fn = os.path.basename(fp)
+
     # save data to two files - latlng and satviews
-    fll = open(fn.split('.')[0] + '_latlng.csv', 'w')
+    fll = open(lldp + fn.split('.')[0] + '_latlng.csv', 'w')
     fll.write('rid,time,lat,lng\n')
-    fsv = open(fn.split('.')[0] + '_sv.csv', 'w')
+    fsv = open(svdp + fn.split('.')[0] + '_sv.csv', 'w')
     fsv.write('rid,time,satnum,elev,azim,SNR\n')
 
     # open file for reading
-    with open(fn, 'r') as fr:
+    with open(nmeadp + fn, 'r') as fr:
 
         # count the lines and save the last time observed
         rcount = 0
