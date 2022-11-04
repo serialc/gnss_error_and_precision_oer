816b460f358102ce21f8937bf3e4a0d31e3148cf
Cyrille MÉDARD DE CHARDON <cyrille.medarddechardon@uni.lu>
2022-11-04 20:02:57 +0100

    a bit of work

--- data_analysis.R
+++ data_analysis.R
@@ -22,24 +22,29 @@ plotSats <- function(source_file) {
   qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
   col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
   
-  png(filename = paste0('images/', strsplit(source_file, "[.]")[[1]][1], "_satpaths.png"), width=500, height=500)
+  # create plot
+  imgsize <- 1000
+  png(filename = paste0('images/', strsplit(source_file, "[.]")[[1]][1], "_satpaths.png"), width=imgsize, height=imgsize)
   par(mar=rep(0,4))
   plot(NULL, xlim=c(-90,90), ylim=c(-90,90), pch=21, cex=sv$SNR/30, bg=col_vector[sv$satnum], asp=1, lwd=3, col="black")
-  lines(c(0,0), c(-90,90))
-  lines(c(-90,90), c(0,0))
-  draw.circle(0,0,90,100)
-  draw.circle(0,0,45,100)
+
+  lines_col <- "gray"
+  lines(c(0,0), c(-90,90), col=lines_col, lwd=2)
+  lines(c(-90,90), c(0,0), col=lines_col, lwd=2)
+  draw.circle(0,0,90,100, border = lines_col, lwd=2)
+  draw.circle(0,0,45,100, border = lines_col, lwd = 2)
   sapply(split(sv, sv$satnum), function(s) {
     points(s$pathx, s$pathy, pch=21, cex=s$SNR/30, col="black", lwd=3)
     points(s$pathx, s$pathy, pch=19, cex=s$SNR/30, col=col_vector[s$satnum], asp=1)
   })
-  shadowtext(c(0,0,0),c(0,-45,-90), labels = c("90°\nZenith","45°","0°\nHorizon"), bg="white", col="black", r=0.1)
-  shadowtext(c(0,70,0,-70), c(70,0,-70,0), labels = c("N", "E", "S", "W"), bg="white", col="black", r=0.1)
+  text(c(0,0,0),c(0,-45,-90), labels = c(" 90°\nZenith"," 45°"," 0°\nHorizon"), bg="white", col="black", cex=2)
+  text(c(0,70,0,-70), c(70,0,-70,0), labels = c("N", "E", "S", "W"), bg="white", col="black", cex=2)
+  #shadowtext(c(0,0,0),c(0,-45,-90), labels = c(" 90°\nZenith"," 45°"," 0°\nHorizon"), bg="white", col="black", r=0.1)
+  #shadowtext(c(0,70,0,-70), c(70,0,-70,0), labels = c("N", "E", "S", "W"), bg="white", col="black", r=0.1)
   dev.off()
 }
-         
+
 # will render figure for all data sets in the path below and save it to 'images'
 for(sf in list.files('processing/sv_data/')) {
   plotSats(sf)
 }
-                                                                                                                                    
